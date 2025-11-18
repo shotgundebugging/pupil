7bf12a004dd181f1dbc2e067c775c76f06533ad2
shotgundebugging <shotgun.debugging@gmail.com>
2025-11-18 23:18:11 +0200

    Add depth video to upload

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -27,6 +27,6 @@ class ScenariosController < ApplicationController
   end
 
   def scenario_params
-    params.require(:scenario).permit(:name, :description, :rgb_video)
+    params.require(:scenario).permit(:name, :description, :rgb_video, :depth_video)
   end
 end
--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,3 +1,4 @@
 class Scenario < ApplicationRecord
   has_one_attached :rgb_video
+  has_one_attached :depth_video
 end
